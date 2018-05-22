7cd53e1ad4054d109f19f47dcd119f07bc495585
vv13 <zwhvv13@gmail.com>
2018-05-22 23:27:51 +0800

    feat: extract-css

--- vue.config.js
+++ vue.config.js
@@ -3,39 +3,65 @@ const glob = require('glob')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const pageEntry = 'main.js'
-const pagePatter = 'src/pages/*'
-const pageHtmlTemplate = 'index.html'
+const config = {
+  pageEntry: 'main.js',
+  pagePatter: 'src/pages/*',
+  pageHtmlTemplate: 'index.html',
+}
+
+const modeDev = process.env.NODE_ENV === 'development'
+
 
 const genEntryPath = () => {
   const entryPath = {}
-  const entries = glob.sync(path.resolve(__dirname, pagePatter))
+  const entries = glob.sync(path.resolve(__dirname, config.pagePatter))
 
   entries.forEach((entry) => {
     const filename = path.basename(entry)
-    entryPath[filename] = `${entry}/${pageEntry}`
+    entryPath[filename] = `${entry}/${config.pageEntry}`
   })
   return entryPath
 }
 
 const genHtmlPlugin = () => {
-  const dirs = glob.sync(path.resolve(__dirname, pagePatter))
+  const dirs = glob.sync(path.resolve(__dirname, config.pagePatter))
   return dirs.map((dir) => {
     const filename = path.basename(dir)
-    return new HtmlWebpackPlugin({
-      template: `${dir}/${pageHtmlTemplate}`,
-      filename: `${filename}/${pageHtmlTemplate}`,
+    return {
+      template: `${dir}/${config.pageHtmlTemplate}`,
+      filename: `${filename}/${config.pageHtmlTemplate}`,
       inject: true,
       showErrors: false,
       chunks: ['manifest', 'vendor', filename],
-    })
+    }
   })
 }
 
+const htmls = genHtmlPlugin()
+
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        return [htmls[0]]
+      })
+    if (!modeDev) {
+      config
+        .plugin('extract-css')
+        .tap(() => {
+          return [{
+            filename: '[name]/css/[name].[contenthash].css',
+            chunkFilename: '[name]/css/[name].[contenthash].css',
+          }]
+          // return [htmls[0]]
+        })
+    }
+  },
   configureWebpack: config => {
     config.entry = genEntryPath()
 
+    const plugins = [...htmls.slice(1).map(e => new HtmlWebpackPlugin(e))]
     if (process.env.NODE_ENV === 'development') {
       config.output = {
         publicPath: '/',
@@ -44,13 +70,14 @@ module.exports = {
       }
     } else {
       config.output = {
+        publicPath: '/',
         path: path.resolve(__dirname, './dist'),
         filename: '[name]/js/[hash].js',
         chunkFilename: '[name]/js/[id].[hash].js',
       }
     }
     return {
-      plugins: [...genHtmlPlugin()]
+      plugins, 
     }
   },
 }
