a737a705aa8a4cef5760ab19b585a5448f0c65be
vv13 <zwhvv13@gmail.com>
2018-06-01 15:05:02 +0800

    refactor: use default option pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const glob = require('glob')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   pageEntry: 'main.js',
@@ -11,71 +9,20 @@ const config = {
 
 const modeDev = process.env.NODE_ENV === 'development'
 
-
-const genEntryPath = () => {
-  const entryPath = {}
-  const entries = glob.sync(path.resolve(__dirname, config.pagePatter))
-
-  entries.forEach((entry) => {
-    const filename = path.basename(entry)
-    entryPath[filename] = `${entry}/${config.pageEntry}`
-  })
-  return entryPath
-}
-
 const genHtmlPlugin = () => {
+  const pages = {}
   const dirs = glob.sync(path.resolve(__dirname, config.pagePatter))
-  return dirs.map((dir) => {
+  dirs.forEach((dir) => {
     const filename = path.basename(dir)
-    return {
+    pages[filename] = {
+      entry: `${dir}/${config.pageEntry}`,
       template: `${dir}/${config.pageHtmlTemplate}`,
       filename: `${filename}/${config.pageHtmlTemplate}`,
-      inject: true,
-      showErrors: false,
     }
   })
+  return pages
 }
 
-const htmls = genHtmlPlugin()
-
 module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        return [htmls[0]]
-      })
-    if (!modeDev) {
-      config
-        .plugin('extract-css')
-        .tap(() => {
-          return [{
-            filename: '[name]/css/[name].[contenthash].css',
-            chunkFilename: '[name]/css/[name].[contenthash].css',
-          }]
-        })
-    }
-  },
-  configureWebpack: config => {
-    config.entry = genEntryPath()
-
-    const plugins = [...htmls.slice(1).map(e => new HtmlWebpackPlugin(e))]
-    if (process.env.NODE_ENV === 'development') {
-      config.output = {
-        publicPath: '/',
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js',
-      }
-    } else {
-      config.output = {
-        publicPath: '/',
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name]/js/[hash].js',
-        chunkFilename: '[name]/js/[id].[hash].js',
-      }
-    }
-    return {
-      plugins, 
-    }
-  },
+  pages: genHtmlPlugin(),
 }
