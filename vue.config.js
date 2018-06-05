4841ad85e8075eae63a9eb44a4666b46c9c842af
vv13 <zwhvv13@gmail.com>
2018-06-05 23:21:50 +0800

    refactor: optimization func

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,33 @@
-const path = require('path')
-const glob = require('glob')
-const fs = require('fs')
+const path = require("path");
+const glob = require("glob");
+const fs = require("fs");
 
 const config = {
-  pageEntry: 'main.js',
-  pagePatter: ['src/pages/*'],
-  pageHtmlTemplate: 'index.html',
-}
+  pageEntry: "main.js",
+  pagePatter: ["src/pages/*"],
+  pageHtmlTemplate: "index.html"
+};
 
-const modeDev = process.env.NODE_ENV === 'development'
+const modeDev = process.env.NODE_ENV === "development";
 
 const genPages = () => {
-  const pages = {}
-  config.pagePatter.map((e) => glob.sync(path.resolve(__dirname, e))).forEach((matches) => {
-    matches.forEach((dir) => {
-      if (!fs.existsSync(`${dir}/${config.pageEntry}`)) return
-      const prefix = 'pages/'
-      const filename = dir.slice(dir.indexOf(prefix) + prefix.length)
-      pages[filename] = {
-        entry: `${dir}/${config.pageEntry}`,
-        template: `${dir}/${config.pageHtmlTemplate}`,
-        filename: `${filename}/${config.pageHtmlTemplate}`,
-      }
-    })
-  })
-  return pages
-}
+  const pages = {};
+  const pageEntries = config.pagePatter.map(e => {
+    const matches = glob.sync(path.resolve(__dirname, e));
+    return matches.filter(match => fs.existsSync(`${match}/${config.pageEntry}`));
+  });
+  Array.prototype.concat.apply([], pageEntries).forEach(dir => {
+    const prefix = "pages/";
+    const filename = dir.slice(dir.indexOf(prefix) + prefix.length);
+    pages[filename] = {
+      entry: `${dir}/${config.pageEntry}`,
+      template: `${dir}/${config.pageHtmlTemplate}`,
+      filename: `${filename}/${config.pageHtmlTemplate}`
+    };
+  });
+  return pages;
+};
 
 module.exports = {
-  pages: genPages(),
-}
+  pages: genPages()
+};
