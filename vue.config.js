ef60359b9495bdf7ba2520411336200d1b632753
vv13 <zwhvv13@gmail.com>
2018-11-07 23:09:12 +0800

    feat: support Nesting

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,39 @@
-const path = require("path");
-const glob = require("glob");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 const config = {
-  entry: "main.js",
-  html: "index.html",
-  pattern: ["src/pages/*"]
+  entry: 'main.js',
+  html: 'index.html',
+  pagesRoot: path.resolve(__dirname, 'src/pages')
+};
+
+const genRoutes = () => {
+  const allRoutes = [];
+
+  const findAllRoutes = (source, routes) => {
+    const files = fs.readdirSync(source);
+    files.forEach(filename => {
+      const fullname = path.join(source, filename);
+      const stats = fs.statSync(fullname);
+      if (!stats.isDirectory()) return;
+      if (fs.existsSync(`${fullname}/${config.html}`)) {
+        routes.push(fullname);
+      } else {
+        findAllRoutes(fullname, routes);
+      }
+    });
+  };
+  findAllRoutes(config.pagesRoot, allRoutes);
+  return allRoutes;
 };
 
 const genPages = () => {
   const pages = {};
-  const pageEntries = config.pattern.map(e => {
-    const matches = glob.sync(path.resolve(__dirname, e));
-    return matches.filter(match => fs.existsSync(`${match}/${config.entry}`));
-  });
-  Array.prototype.concat.apply([], pageEntries).forEach(dir => {
-    const filename = dir.split(path.sep).pop();
+  genRoutes().forEach(route => {
+    const filename = route.split('pages/').pop();
     pages[filename] = {
-      entry: `${dir}/${config.entry}`,
-      template: `${dir}/${config.html}`,
+      entry: `${route}/${config.entry}`,
+      template: `${route}/${config.html}`,
       filename: `${filename}/${config.html}`
     };
   });
@@ -31,25 +46,26 @@ module.exports = {
   productionSourceMap: false,
   pages,
   chainWebpack: config => {
+    // remove prefetch, use import(/* webpackPrefetch: true */ './someAsyncComponent.vue')
     Object.keys(pages).forEach(entryName => {
       config.plugins.delete(`prefetch-${entryName}`);
     });
-    if (process.env.NODE_ENV === "production") {
-      config.plugin("extract-css").tap(() => [
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('extract-css').tap(() => [
         {
-          filename: "[name]/css/[name].[contenthash:8].css",
-          chunkFilename: "[name]/css/[name].[contenthash:8].css"
+          filename: '[name]/css/[name].[contenthash:8].css',
+          chunkFilename: '[name]/css/[name].[contenthash:8].css'
         }
       ]);
     }
   },
   configureWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === 'production') {
       config.output = {
-        path: path.join(__dirname, "./dist"),
-        filename: "[name]/js/[name].[contenthash:8].js",
-        publicPath: "/",
-        chunkFilename: "[name]/js/[name].[contenthash:8].js"
+        path: path.join(__dirname, './dist'),
+        filename: '[name]/js/[name].[contenthash:8].js',
+        publicPath: '/',
+        chunkFilename: '[name]/js/[name].[contenthash:8].js'
       };
     }
   }
